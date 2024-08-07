10e7ddd94d97357c978b6f0f5bb424aa51ae5908
Amirhossein Banavi <ahbanavi@gmail.com>
2024-08-07 19:52:31 +0330

    refactor(user): simpilify edit

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
-use Illuminate\Database\Eloquent\Model;
 
 class EditUser extends EditRecord
 {
@@ -16,14 +15,5 @@ class EditUser extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
-    }
-
-    protected function handleRecordUpdate(Model $record, array $data): Model
-    {
-        if ($data['is_admin'] === false && auth()->user()->is($record)) {
-            $data['is_admin'] = true;
-        }
-
-        return parent::handleRecordUpdate($record, $data);
     }
 }
