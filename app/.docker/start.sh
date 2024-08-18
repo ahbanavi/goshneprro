81b6b9a9ef040464ec19b6406bb97a4835d86588
Amirhossein Banavi <ahbanavi@gmail.com>
2024-08-18 13:50:26 +0330

    feat(docker): add Dockerfile

---
+++ app/.docker/start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+role=${CONTAINER_ROLE:-app}
+
+env=${APP_ENV:-production}
+
+if [ "$env" != "local" ]; then
+    echo "Caching configuration..."
+    (cd /var/www/html && php artisan optimize:clear && php artisan optimize)
+fi
+
+if [ "$role" = "app" ]; then
+
+    exec php-fpm
+
+elif [ "$role" = "queue" ]; then
+
+    echo "Running the queue..."
+    php /var/www/html/artisan queue:work --sleep=15 --tries=5 --max-time=3600
+
+elif [ "$role" = "scheduler" ]; then
+
+    echo "Scheduler role"
+    while true
+    do
+      php /var/www/html/artisan schedule:run --no-interaction >> /dev/null 2>&1 &
+      sleep 60
+    done
+
+else
+    echo "Could not match the container role \"$role\""
+    exit 1
+fi
