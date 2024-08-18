b0eef9335ff34513db8a3bf65acf086fd81bd047
Amirhossein Banavi <ahbanavi@gmail.com>
2024-08-18 16:11:23 +0330

    fix(ssl): force ssl in stage and production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -30,5 +32,9 @@ class AppServiceProvider extends ServiceProvider
                 Limit::perMinute(300)->by($notifiable->notifiables->first()->tg_chat_id),
             ];
         });
+
+        if (App::environment(['staging', 'production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
